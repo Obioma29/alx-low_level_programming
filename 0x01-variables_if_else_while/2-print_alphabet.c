fc656b29fe4bacbaaa37544ee471e4b5a07753d7
Obioma29 <obiomachinwuko@yahoo.com>
2022-12-11 14:29:04 -0800

    Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Alphabets in lower case followed by new line
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	return (0);
+}
