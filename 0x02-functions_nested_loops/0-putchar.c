07ce9d8e76058af48c36277bc7266782e10fce81
Obioma29 <obiomachinwuko@yahoo.com>
2022-12-14 09:19:38 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main- Entry point
+ * main - print _putchar
  * Description: Print putchar
- * Return: Always success (0)
+ * Return: 0
  */
 
 int main(void)
 
 {
-
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
-
 }
