3eb371e5a07f28e4bb7b3f56cf7f7fdf92aa186e
Obioma29 <obiomachinwuko@yahoo.com>
2022-12-14 11:08:14 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ * @c: Function that check for lower case letter
+ * Return: Always (0)
+ */
+
+int _islower(int c)
+
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
