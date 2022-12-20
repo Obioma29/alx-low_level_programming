9184d44a9fbfe7d792983ece3d0f041b537d4ae7
Obioma29 <obiomachinwuko@yahoo.com>
2022-12-20 04:25:44 -0800

    prints a string, in reverse, followed by a new line

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -16,10 +16,10 @@ void print_rev(char *s)
 		s++;
 	}
 	s--;
-	for (o = longi; o > 0; o++)
+	for (o = longi; o > 0; o--)
 	{
-		putchar(*s);
+		_putchar(*s);
 		s--;
 	}
-	putchar('\n');
+	_putchar('\n');
 }
