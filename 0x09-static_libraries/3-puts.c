a6343021c6bd9dcb3a109cf0cf3e31b04ea13e33
Obioma29 <obiomachinwuko@yahoo.com>
2023-01-11 00:22:41 -0800

    A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,1 +1,19 @@
+#include "main.h"
+/**
+ * _puts - Entry point
+ * Description: prints a string, followed by a new line
+ * to stdout
+ * @str: string to print
+ */
 
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_puts("*str++");
+	}
+	_puts("'\n'");
+}
+int main()
+{
+}
