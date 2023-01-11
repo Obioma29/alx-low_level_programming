4205b07c17411756bdcb29a87051d49143d4ca69
Obioma29 <obiomachinwuko@yahoo.com>
2023-01-11 04:00:17 -0800

    Statics

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,1 +1,23 @@
+#include "main.h"
+#include "stdio.h"
 
+/**
+ * _puts - Entry point
+ *
+ * Description: prints a string, followed by a new line
+ *
+ * to stdout
+ *
+ * @str: string to print
+ * 
+ */
+
+void _puts(char *str)
+
+{
+	while (*str != '\0')
+	{
+	putchar(*str++);
+	}
+	putchar('\n');
+}
